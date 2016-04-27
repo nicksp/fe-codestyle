e27fc0a08a18508f89c81584b9570668e08fddc5
Nick Plekhanov <nickatgit@yandex.com>
2016-04-27 23:29:29 +0200

    eslint-config: Update React ESLint config file.
    Remove a set of React specific rules in favour of recommended configuration that enforce React good practices.
    
    Apart from that update the whole config to reflect ESLint 2.0 changes.

--- linters/eslint-config/react.js
+++ linters/eslint-config/react.js
@@ -3,16 +3,16 @@
     "react"
   ],
 
-  "ecmaFeatures": {
-    "jsx": true
+  "extends": "plugin:react/recommended"
+
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    }
   },
 
   "rules": {
-    "react/jsx-uses-react": 1,
-    "react/jsx-no-undef": 2,
-    "react/no-is-mounted": 2,
     "react/wrap-multilines": 2,
-    "react/display-name": [ 1, {"acceptTranspilerName": true }],
     "react/forbid-prop-types": [1, {"forbid": "any"}],
     "react/jsx-boolean-value": 1,
     "react/jsx-closing-bracket-location": 0,
@@ -21,21 +21,13 @@
     "react/jsx-key": 1,
     "react/jsx-max-props-per-line": 0,
     "react/jsx-no-bind": 0,
-    "react/jsx-no-duplicate-props": 1,
     "react/jsx-no-literals": 0,
     "react/jsx-pascal-case": 1,
     "react/jsx-sort-prop-types": 0,
     "react/jsx-sort-props": 0,
-    "react/jsx-uses-vars": 1,
-    "react/no-danger": 1,
-    "react/no-did-mount-set-state": 1,
-    "react/no-did-update-set-state": 1,
-    "react/no-direct-mutation-state": 1,
     "react/no-multi-comp": 1,
     "react/no-set-state": 1,
-    "react/no-unknown-property": 1,
     "react/prefer-es6-class": 1,
-    "react/react-in-jsx-scope": 1,
     "react/require-extension": 1,
     "react/self-closing-comp": 1,
     "react/sort-comp": 1
